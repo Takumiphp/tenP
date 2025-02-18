be8dc28c1e34d96bfb496f4c2abac7574b52f97c
takumi.yasuda <takumi.yasuda@bco.co.jp>
2025-02-18 10:05:10 +0900

    レイアウト作成、セレクトボックス等

--- drow.js
+++ drow.js
@@ -1,14 +1,23 @@
+let cnv;
 function setup() {
-    createCanvas(400, 400); // 400x400のキャンバスを作成
-    background(240); // 背景をグレーに設定
-}
+  cnv = createCanvas(600, 450);
+  cnv.parent("canvas-container");
+  cnv.background(200);
+  }
+  
+  function draw() {
+  }
+  
+  function keyPressed() {
+    if (key === 'd') {
+      removeCanvas();
+    }
+  }
 
-function draw() {
-    fill(100, 200, 255); // 塗りつぶし色を水色に設定
-    stroke(0); // 線の色を黒に設定
-    strokeWeight(2); // 線の太さを設定
-    ellipse(200, 200, 100, 100); // 円を描画
-
-    fill(255, 100, 100);
-    rect(150, 250, 100, 50); // 四角形を描画
-}
+  function removeCanvas(){
+    cnv.remove();
+    cnv = createCanvas(600, 450);
+    cnv.parent("canvas-container");
+    cnv.background(200);
+  }
+  
